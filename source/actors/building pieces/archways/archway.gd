1093c05b94cc7ae18a705de90918e3a3e321c87f
TheWoodenTree <94566175+TheWoodenTree@users.noreply.github.com>
2024-05-09 21:10:10 -0400

    Update large archway to be more in line with current archways

--- source/actors/building pieces/archways/archway.gd
+++ source/actors/building pieces/archways/archway.gd
@@ -5,10 +5,11 @@ extends Node3D
 @export var length: float = 1.0 : set = _set_length
 @export var xy_scale: Vector2 = Vector2.ONE : set = _set_xy_scale
 @export_enum("Prison", "Polished") var style: String = "Prison" : set = _set_style
-@export var has_frame: bool = true
+@export var has_frame: bool = true : set = _set_has_frame
 
 var archway: MeshInstance3D = null
 var frame: MeshInstance3D = null
+var frame2: MeshInstance3D = null
 var static_body: StaticBody3D = null
 
 static var prison_bricks_mat: StandardMaterial3D = load("res://source/assets/materials/bricks/bricks.tres")
@@ -20,7 +21,7 @@ static var polished_frame_mat: StandardMaterial3D = load("res://source/assets/ma
 func _ready() -> void:
 	archway = find_child("archway")
 	frame = find_child("frame")
-	static_body = find_child("StaticBody3D")
+	static_body = find_child("static_body")
 	if archway:
 		archway.scale = Vector3(xy_scale.x, xy_scale.y, length)
 	if static_body:
@@ -63,3 +64,13 @@ func update_style():
 		archway.mesh.surface_set_material(0, archway_mat)
 	if frame:
 		frame.mesh.surface_set_material(0, frame_mat)
+
+
+func _set_has_frame(has_frame_: bool):
+	has_frame = has_frame_
+	update_has_frame()
+
+
+func update_has_frame():
+	if has_frame:
+		frame.visible = true
