de845568b3e0365c02c9ee9d5189323546229d9f
TheWoodenTree <94566175+TheWoodenTree@users.noreply.github.com>
2024-06-14 21:44:51 -0400

    Add fly swarm particles + sound

--- source/actors/player/random_sound.gd
+++ source/actors/player/random_sound.gd
@@ -6,12 +6,18 @@ extends Node3D
 @export var max_sound_y_distance: float = 4.0
 @export var mean_sound_interval: float = 20.0
 @export var sound_interval_variance: float = 5.0
+@export var playing: bool = true : set = _set_playing
 
 @onready var sound_player = $sound_player
 @onready var sound_timer = $sound_timer
 
 
 func _ready():
+	if playing:
+		start_timer()
+
+
+func start_timer():
 	sound_timer.wait_time = mean_sound_interval + randf_range(-sound_interval_variance, sound_interval_variance)
 	sound_timer.start()
 
@@ -29,5 +35,12 @@ func _on_sound_timer_timeout():
 	sound_player.stream = stream
 	sound_player.play()
 	
-	sound_timer.wait_time = mean_sound_interval + randf_range(-sound_interval_variance, sound_interval_variance)
-	sound_timer.start()
+	start_timer()
+
+
+func _set_playing(playing_: bool):
+	playing = playing_
+	if playing:
+		start_timer()
+	else:
+		sound_timer.stop()
