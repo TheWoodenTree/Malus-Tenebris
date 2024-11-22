734f334862407a915ee33d91723581d7a47d4a2e
TheWoodenTree <94566175+TheWoodenTree@users.noreply.github.com>
2024-11-22 05:34:00 -0500

    Update prologue

--- source/assets/prologue/prologue.gd
+++ source/assets/prologue/prologue.gd
@@ -10,18 +10,22 @@ signal finished
 func _ready() -> void:
 	rect.z_index = 0
 	for card in cont.get_children():
+		# Set all text to have no chars showing
 		for label: Label in card.get_children():
 			label.visible_characters = 0
 		card.visible = true
 			
+		# Reveal text
 		for label: Label in card.get_children():
 			await reveal_label_text(label, 0.05)
 			await get_tree().create_timer(1.5, false).timeout
 		
+		# Fade to black
 		await get_tree().create_timer(2.5, false).timeout
 		var tween: Tween = get_tree().create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 		tween.tween_property(rect, "color", Color.BLACK, 2.5)
 		
+		# Prepare for next card to show
 		await tween.finished
 		card.visible = false
 		rect.color = Color(0.0, 0.0, 0.0, 0.0)
@@ -36,6 +40,8 @@ func reveal_label_text(label: Label, reveal_interval: float):
 	var idx: int = 0
 	while label.visible_characters < char_count:
 		label.visible_characters += 1
+		#if label.text[label.visible_characters - 1] == " ":
+		#	continue
 		if idx % 2 == 0:
 			text_click_player.play()
 		idx += 1
