eccbaf4233755b83d27f4f25874d7b0a86461875
TheWoodenTree <94566175+TheWoodenTree@users.noreply.github.com>
2025-03-18 19:41:49 -0400

    Debug

--- source/actors/interactables/lower_prison_entrance_shelf.gd
+++ source/actors/interactables/lower_prison_entrance_shelf.gd
@@ -4,4 +4,4 @@ extends Moveable
 
 func _ready() -> void:
 	super()
-	GlobalSignals.apothecary_door_closed.connect(func(): enable_highlight_sheen = true; enable_sheen)
+	GlobalSignals.apothecary_door_closed.connect(func(): enable_highlight_sheen = true; enable_sheen())
